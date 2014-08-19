975becfe6031b7e19e615978470db799a134ea9a
Lien Chiang <xsoameix@gmail.com>
2014-08-19 10:27:44 +0800

    Complete 100 % of func_403270_teleport.rb

--- doc/func_403270_teleport.rb
+++ doc/func_403270_teleport.rb
@@ -2,30 +2,31 @@
 def func_403270_teleport(global, teleported_id, teleport_to)
   # or edx,FFFFFFFF
   @closest_object_id = -1
+  @farthest_object_id = -1
   case teleport_to
   when teleport_to_enemy
     @min_distance = 10000
-    object_ptr = &global->objects
+    @object_ptr = &global->objects
 
     # the condition of the loop
     # 0040330E
-    for (object_i = 0;
-         object_i < max_objects_size;
-         object_i++, object_ptr++)
+    for (object_id = 0;
+         object_id < max_objects_size;
+         object_id++, @object_ptr++)
       # 004032A3
-      object_exists = global->is_object_exists[object_i]
-      object = * object_ptr
+      object_exists = global->is_object_exists[object_id]
+      object = * @object_ptr
       teleported = global->objects[teleported_id]
       distance =
         abs(object->z - teleported->z) +
         abs(object->x - teleported->x)
-      if (object_i != teleported_id and
+      if (object_id != teleported_id and
           object_exists and
           object->file->type == character_type and
           object->team != teleported->team and
           object->hp > 0 and
           distance < @min_distance)
-        @closest_object_id = object_i
+        @closest_object_id = object_id
         @min_distance = distance
       end
     end
@@ -45,6 +46,8 @@ def func_403270_teleport(global, teleported_id, teleport_to)
       teleported = global->objects[teleported_id]
       closest_object = global->objects[@closest_object_id]
       closest_object_x = closest_object->x
+
+      # confront to enemy
       if teleported->facing == facing_right
         closest_object_x -= 120
       else
@@ -62,6 +65,62 @@ def func_403270_teleport(global, teleported_id, teleport_to)
     end
   when teleport_to_team
     # 00403396
+    @max_distance = -1
+    @object_ptr = &global->objects
+
+    # the condition of the loop
+    # 0040341E
+    for (object_id = 0;
+         object_id < max_objects_size;
+         object_id++, @object_ptr++)
+      # 004033B3
+      object_exists = global->is_object_exists[object_id]
+      object = * @object_ptr
+      teleported = global->objects[teleported_id]
+      distance =
+        abs(object->z - teleported->z) +
+        abs(object->x - teleported->x)
+      if (object_id != teleported_id and
+          object_exists and
+          object->file->type == character_type and
+          object->team == teleported->team and
+          object->hp > 0 and
+          distance > @max_distance)
+        @farthest_object_id = object_id
+      end
+    end
+    if @farthest_object_id == -1
+      # 00403327
+      teleported = global->objects[teleported_id]
+      teleported->y = 0
+      teleported = global->objects[teleported_id]
+      teleported->y_position = teleported->y.to_f
+    else
+      teleported = global->objects[teleported_id]
+      teleported->y = 0
+      farthest_object = global->objects[@farthest_object_id]
+      teleported = global->objects[teleported_id]
+      teleported->z = farthest_object->z + 1
+      teleported = global->objects[teleported_id]
+      farthest_object = global->objects[@farthest_object_id]
+      farthest_object_x = farthest_object->x
+
+      # confront to team
+      if teleported->facing == facing_right
+        farthest_object_x -= 60
+      else
+        # 0040347F
+        farthest_object_x += 60
+      end
+      # 00403482
+      teleported->x = farthest_object_x
+      teleported = global->objects[teleported_id]
+      teleported = global->objects[teleported_id]
+      teleported = global->objects[teleported_id]
+      teleported->x = teleported->x_position
+      teleported->y = teleported->y_position
+      teleported->z = teleported->z_position
+    end
   else
     return
   end
--- func_403270_teleport.rb
+++ func_403270_teleport.rb
@@ -11,6 +11,7 @@ def func_403270_teleport(global, teleported_id, teleport_to)
   # mov dword ptr ss:[esp+10],edx
   # jnz 00403396
   @closest_object_id = -1
+  @farthest_object_id = -1
   case teleport_to
   when teleport_to_enemy
     # mov edx,dword ptr ss:[esp+1C]
@@ -19,7 +20,7 @@ def func_403270_teleport(global, teleported_id, teleport_to)
     # xor ebx,ebx
     # mov dword ptr ss:[esp+20],eax
     @min_distance = 10000
-    object_ptr = &global->objects
+    @object_ptr = &global->objects
 
     # the condition of the loop
     # 0040330E
@@ -27,22 +28,22 @@ def func_403270_teleport(global, teleported_id, teleport_to)
     # add ebx,1
     # cmp ebx,190
     # jl short 004032A3
-    for (object_i = 0;
-         object_i < max_objects_size;
-         object_i++, object_ptr++)
+    for (object_id = 0;
+         object_id < max_objects_size;
+         object_id++, @object_ptr++)
       # 004032A3
       # cmp ebx,edx
       # je short 0040330E
       # cmp byte ptr ds:[ebx+ecx+4],0
       # je short 0040330E
-      object_exists = global->is_object_exists[object_i]
+      object_exists = global->is_object_exists[object_id]
 
       # mov eax,dword ptr ss:[esp+20]
       # mov edi,dword ptr ds:[eax]
       # mov eax,dword ptr ds:[edi+368]
       # cmp dword ptr ds:[eax+6F8],0
       # jnz short 0040330E
-      object = * object_ptr
+      object = * @object_ptr
 
       # mov ebp,dword ptr ds:[ecx+edx*4+194]
       # mov eax,dword ptr ds:[edi+364]
@@ -68,7 +69,7 @@ def func_403270_teleport(global, teleported_id, teleport_to)
       distance =
         abs(object->z - teleported->z) +
         abs(object->x - teleported->x)
-      if (object_i != teleported_id and
+      if (object_id != teleported_id and
           object_exists and
           object->file->type == character_type and
           object->team != teleported->team and
@@ -76,7 +77,7 @@ def func_403270_teleport(global, teleported_id, teleport_to)
           distance < @min_distance)
         # mov dword ptr ss:[esp+10],ebx
         # mov dword ptr ss:[esp+14],esi
-        @closest_object_id = object_i
+        @closest_object_id = object_id
         @min_distance = distance
       end
     end
@@ -117,6 +118,8 @@ def func_403270_teleport(global, teleported_id, teleport_to)
       teleported = global->objects[teleported_id]
       closest_object = global->objects[@closest_object_id]
       closest_object_x = closest_object->x
+
+      # confront to enemy
       if teleported->facing == facing_right
         # sub eax,78
         # jmp 00403482
@@ -150,6 +153,141 @@ def func_403270_teleport(global, teleported_id, teleport_to)
     # 00403396
     # cmp eax,2
     # jnz 004034CC
+
+    # mov dword ptr ss:[esp+14],edx
+    # lea edx,dword ptr ds:[ecx+194]
+    # mov dword ptr ss:[esp+20],edx
+    # mov edx,dword ptr ss:[esp+1C]
+    # xor ebx,ebx
+    @max_distance = -1
+    @object_ptr = &global->objects
+
+    # the condition of the loop
+    # 0040341E
+    # add dword ptr ss:[esp+20],4
+    # add ebx,1
+    # cmp ebx,190
+    # jl short 004033B3
+    for (object_id = 0;
+         object_id < max_objects_size;
+         object_id++, @object_ptr++)
+      # 004033B3
+      # cmp ebx,edx
+      # je short 0040341E
+
+      # cmp byte ptr ds:[ebx+ecx+4],0
+      # je short 0040341E
+      object_exists = global->is_object_exists[object_id]
+
+      # mov eax,dword ptr ss:[esp+20]
+      # mov edi,dword ptr ds:[eax]
+      # mov eax,dword ptr ds:[edi+368]
+      # cmp dword ptr ds:[eax+6F8],0
+      # jnz short 0040341E
+      object = * @object_ptr
+
+      # mov ebp,dword ptr ds:[ecx+edx*4+194]
+      # mov eax,dword ptr ds:[edi+364]
+      # cmp eax,dword ptr ss:[ebp+364]
+      # jnz short 0040341E
+      teleported = global->objects[teleported_id]
+
+      # cmp dword ptr ds:[edi+2FC],0
+      # jle short 0040341E
+
+      # mov eax,dword ptr ds:[edi+18]
+      # sub eax,dword ptr ss:[ebp+18]
+      # push eax
+      # call 004034E0
+      # mov esi,eax
+      # mov eax,dword ptr ds:[edi+10]
+      # sub eax,dword ptr ss:[ebp+10]
+      # push eax
+      # call 004034E0
+      # add esi,eax
+      # add esp,8
+      # cmp esi,dword ptr ss:[esp+14]
+      # jle short 0040341E
+      distance =
+        abs(object->z - teleported->z) +
+        abs(object->x - teleported->x)
+      if (object_id != teleported_id and
+          object_exists and
+          object->file->type == character_type and
+          object->team == teleported->team and
+          object->hp > 0 and
+          distance > @max_distance)
+        # mov dword ptr ss:[esp+10],ebx
+        # mov dword ptr ss:[esp+14],esi
+        @farthest_object_id = object_id
+      end
+    end
+    # mov eax,dword ptr ss:[esp+10]
+    # cmp eax,-1
+    # je 00403327
+    if @farthest_object_id == -1
+      # 00403327
+      # mov eax,dword ptr ds:[ecx+edx*4+194]
+      # mov dword ptr ds:[eax+14],0
+      # mov eax,dword ptr ds:[ecx+edx*4+194]
+      # fild dword ptr ds:[eax+14]
+      # fstp qword ptr ds:[eax+60]
+      # jmp 004034AC
+      teleported = global->objects[teleported_id]
+      teleported->y = 0
+      teleported = global->objects[teleported_id]
+      teleported->y_position = teleported->y.to_f
+    else
+      # mov esi,dword ptr ds:[ecx+edx*4+194]
+      # mov dword ptr ds:[esi+14],0
+      # mov esi,dword ptr ds:[ecx+eax*4+194]
+      # mov esi,dword ptr ds:[esi+18]
+      # mov edi,dword ptr ds:[ecx+edx*4+194]
+      # add esi,1
+      # mov dword ptr ds:[edi+18],esi
+      # mov esi,dword ptr ds:[ecx+edx*4+194]
+      # cmp byte ptr ds:[esi+80],0
+      # mov eax,dword ptr ds:[ecx+eax*4+194]
+      # mov eax,dword ptr ds:[eax+10]
+      # jnz short 0040347F
+      teleported = global->objects[teleported_id]
+      teleported->y = 0
+      farthest_object = global->objects[@farthest_object_id]
+      teleported = global->objects[teleported_id]
+      teleported->z = farthest_object->z + 1
+      teleported = global->objects[teleported_id]
+      farthest_object = global->objects[@farthest_object_id]
+      farthest_object_x = farthest_object->x
+
+      # confront to team
+      if teleported->facing == facing_right
+        # sub eax,3C
+        # jmp short 00403482
+        farthest_object_x -= 60
+      else
+        # 0040347F
+        # add eax,3C
+        farthest_object_x += 60
+      end
+      # 00403482
+      # mov dword ptr ds:[esi+10],eax
+      # mov eax,dword ptr ds:[ecx+edx*4+194]
+      # fild dword ptr ds:[eax+10]
+      # fstp qword ptr ds:[eax+58]
+      # mov eax,dword ptr ds:[ecx+edx*4+194]
+      # fild dword ptr ds:[eax+14]
+      # fstp qword ptr ds:[eax+60]
+      # mov eax,dword ptr ds:[ecx+edx*4+194]
+      # fild dword ptr ds:[eax+18]
+      # fstp qword ptr ds:[eax+68]
+      teleported->x = farthest_object_x
+      teleported = global->objects[teleported_id]
+      teleported = global->objects[teleported_id]
+      teleported = global->objects[teleported_id]
+      teleported->x = teleported->x_position
+      teleported->y = teleported->y_position
+      teleported->z = teleported->z_position
+    end
   else
     return
   end
